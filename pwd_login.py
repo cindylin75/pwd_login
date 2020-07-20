87dafc1def2e937f15e0853dd688444717da1cbd
Cindy Lin <u588070544@gmail.com>
2020-07-20 13:26:54 -0700

    add pwd coding

---
+++ pwd_login.py
@@ -0,0 +1,7 @@
+pwd = input('please insert your password:')
+pwd = 'a123456'
+while true
+if pwd = 'a123456':
+	print('log in success!')
+    break
+else print('the password is worng, you have 2 chance')
