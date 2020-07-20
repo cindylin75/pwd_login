581bfae2c5f2176882696b49829d7173bdacfff5
Cindy Lin <u588070544@gmail.com>
2020-07-20 14:13:17 -0700

    change advance part

--- pwd_login.py
+++ pwd_login.py
@@ -1,12 +1,16 @@
 password = 'a123456'
 i = 3
 while True:
+	i = i - 1
 	pwd = input('Please insert the password:')
 	if pwd == password:
 		print('Log in success!')
 		break
 	else:
-		i = i - 1
-		print('The password is worong. You have', i ,'chance')
-		if i == 0:
+		print('The password is wrong.')
+		if i > 0:
+			print('You have', i ,'chance')
+		else:
+			print('You have no chance! Please email your protector')
 			break
+			
